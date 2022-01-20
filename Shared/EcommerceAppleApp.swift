1a7e7f56ccd287fd1a5b465b8f80594572906d4e
jacobkosmart <jacobkosmart@gmail.com>
2022-01-20 17:02:27 +0900

    Initial Commit

---
+++ Shared/EcommerceAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppleApp.swift
+//  Shared
+//
+//  Created by Jacob Ko on 2022/01/20.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
